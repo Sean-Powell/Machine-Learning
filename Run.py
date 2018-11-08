62b50019ebcbd8fcb3cd49abfbc9e99ba9e236bb
seanp <seanpowell54@gmail.com>
2018-11-08 23:57:06 +0100

    added graph drawing function

--- Run.py
+++ Run.py
@@ -32,10 +32,19 @@ def _parseData(lines):
     return data_set
 
 
+def _drawGraph(amount_of_fields):
+    if amount_of_fields <= 2:
+       print("2D graph")
+    elif amount_of_fields == 3:
+        print("3D graph")
+    else:
+        print("too many fields selected")
+
+
 def _askUser(length):
     print("Please input what fields you would like to use")
-    print("you can enter up to 3 with there being ", length, " fields from 1 -", length)
-    print("The fields being 1. Sepal Length, 2. Sepal Width, 3. Petal Length, 4. Petal Width, 5. Class")
+    print("you can enter up to 3 with there being ", (length - 1), " fields from 1 -", (length - 1))
+    print("The fields being 1. Sepal Length, 2. Sepal Width, 3. Petal Length, 4. Petal Width")
     user_in = input("Please separate your inputs of the fields you want via a space\n")
     split_user = user_in.split(" ")
     return split_user
@@ -44,6 +53,8 @@ def _askUser(length):
 def _run():
     data_set = _parseData(_readFile("irisData.txt"))
     user_input = _askUser(data_set[0].data_size)
+    amount_of_fields = len(user_input)
+    _drawGraph(amount_of_fields)
     for input in user_input:
         print(input)
 
